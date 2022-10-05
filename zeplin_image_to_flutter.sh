bab3979cf76fd77a07c5792b57cbdf759665247d
happinessee <gygur1206@gmail.com>
2022-10-05 16:00:53 +0900

    add shellscript/ auto select images directory

---
+++ zeplin_image_to_flutter.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mv assets/images/drawable-mdpi/* assets/images/;
+mv assets/images/drawable-hdpi/* assets/images/1.5x/;                                                                                                                                                                         15:44:47
+mv assets/images/drawable-xhdpi/* assets/images/2.0x/;                                                                                                                                                                         15:45:23
+mv assets/images/drawable-xxhdpi/* assets/images/3.0x/;                                                                                                                                                                       15:45:33
+mv assets/images/drawable-xxxhdpi/* assets/images/4.0x/;   
